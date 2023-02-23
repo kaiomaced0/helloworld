2203685cc16392e18501859ac84a8d063c0ab550
Kaio Macedo <75695233+kaiomaced0@users.noreply.github.com>
2023-02-23 13:44:00 -0300

    CRUD e Search Produto e Usuario

--- src/main/java/br/unitins/resource/ProdutoResource.java
+++ src/main/java/br/unitins/resource/ProdutoResource.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -14,7 +17,7 @@ import br.unitins.model.Produto;
 
 @Path("/produtos")
 public class ProdutoResource {
-
+    
     @GET
     @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
@@ -31,4 +34,45 @@ public class ProdutoResource {
         return produto;
     }
     
+    @DELETE
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Produto del(@PathParam("id") Long id) {
+
+        Produto entity = Produto.findById(id);
+        entity.delete();
+        return entity;
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Produto update(@PathParam("id") Long id, Produto produto){
+        
+        Produto entity = Produto.findById(id);
+
+        entity.setPreco(produto.getPreco());
+        entity.setNome(produto.getNome());
+
+        return entity;
+    }
+
+    @GET
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Produto search(@PathParam("id") Long id) {
+
+        Produto entity = Produto.findById(id);
+
+        return entity;
+    }
+
+
+
 }
